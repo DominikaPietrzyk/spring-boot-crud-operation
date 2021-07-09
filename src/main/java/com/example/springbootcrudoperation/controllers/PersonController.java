03f6e02977ee9f2f559f8d56b1029f3872f7c38f
DominikaPietrzyk <72759164+DominikaPietrzyk@users.noreply.github.com>
2021-07-09 09:24:11 +0200

    Controller update

--- src/main/java/com/example/springbootcrudoperation/controllers/PersonController.java
+++ src/main/java/com/example/springbootcrudoperation/controllers/PersonController.java
@@ -1,9 +1,15 @@
 package com.example.springbootcrudoperation.controllers;
 
 
+import com.example.springbootcrudoperation.dto.PersonDto;
+import com.example.springbootcrudoperation.model.Person;
 import com.example.springbootcrudoperation.service.PersonService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequestMapping("api/person")
 @RestController
@@ -13,6 +19,22 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
+    @GetMapping("/{personId}")
+    public ResponseEntity<PersonDto> getPerson(@PathVariable("personId")UUID personId){
+        return new ResponseEntity<>(personService.getPersonById(personId), HttpStatus.ACCEPTED);  //HttpStatus.OK ?
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody PersonDto personDto){
+        PersonDto personDto1 =personService.saveNewPerson(personDto);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location","/api/person/"+personDto1.getId().toString());
+
+        return new ResponseEntity(httpHeaders,HttpStatus.CREATED);
+    }
+
 
 
 }
